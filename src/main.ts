6bcf87044c41c078854bc4ece98394d186e1144f
Artur Zakharov <118992316+arthurzakharov@users.noreply.github.com>
2025-04-10 10:31:36 +0200

    Update exports

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
+// COMPONENTS
 export { default as Calendar } from './components/calendar/calendar.component';
 export { default as Checkbox } from './components/checkbox/checkbox.component';
 export { default as Dropdown } from './components/dropdown/dropdown.component';
+export { default as InputControl } from './components/input-control/input-control.component';
+export { default as InputMasked } from './components/input-masked/input-masked.component';
 export { default as LandingPage } from './components/landing-page/landing-page.component';
-export { default as LandingPageLoader } from './components/landing-page/components/landing-page-loader/landing-page-loader.component';
 export { default as Logos } from './components/logos/logos.component';
 export { default as NavigationButtons } from './components/navigation-buttons/navigation-buttons.component';
 export { default as OrderedList } from './components/ordered-list/ordered-list.component';
+// LANDING PAGE
+export { default as LandingPageAccordion } from './components/landing-page/components/landing-page-accordion/landing-page-accordion.component';
+export { default as LandingPageAdvantages } from './components/landing-page/components/landing-page-advantages/landing-page-advantages.component';
+export { default as LandingPageButton } from './components/landing-page/components/landing-page-button/landing-page-button.component';
+export { default as LandingPageContact } from './components/landing-page/components/landing-page-contact/landing-page-contact.component';
+export { default as LandingPageFooter } from './components/landing-page/components/landing-page-footer/landing-page-footer.component';
+export { default as LandingPageHowTo } from './components/landing-page/components/landing-page-how-to/landing-page-how-to.component';
+export { default as LandingPageLoader } from './components/landing-page/components/landing-page-loader/landing-page-loader.component';
+export { default as LandingPageLogoBoard } from './components/landing-page/components/landing-page-logo-board/landing-page-logo-board.component';
+export { default as LandingPagePlayer } from './components/landing-page/components/landing-page-player/landing-page-player.component';
+export { default as LandingPageQuestions } from './components/landing-page/components/landing-page-questions/landing-page-questions.component';
+export { default as LandingPageReview } from './components/landing-page/components/landing-page-review/landing-page-review.component';
+export { default as LandingPageSidebar } from './components/landing-page/components/landing-page-sidebar/landing-page-sidebar.component';
+export { default as LandingPageSuccessBox } from './components/landing-page/components/landing-page-success-box/landing-page-success-box.component';
+// ICONS
+export { default as CheckIcon } from './icons/check/check.component';
+export { default as CheckCircleIcon } from './icons/check-circle/check-circle.component';
+export { default as ChevronIcon } from './icons/chevron/chevron.component';
+export { default as CrossIcon } from './icons/cross/cross.component';
+export { default as ExclamationIcon } from './icons/exclamation/exclamation.component';
+export { default as ExclamationTriangleIcon } from './icons/exclamation-triangle/exclamation-triangle.component';
+export { default as MagnifyingGlassIcon } from './icons/magnifying-glass/magnifying-glass.component';
+export { default as QuestionIcon } from './icons/question/question.component';
